695a9f5e0e903a2056f3489e61959b9e7a600296
Ioan Scurtu <ioan.scurtu120@gmail.com>
2019-08-21 14:56:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,27 +1,5 @@
-// write your code below!
-var name = "Susan"
+var name = "Joe"
 var height = 74
 var message = ` is ${height} inches tall`
 
-module.exports = {name, height, message};
-
-var index = require("/index.js");
-
-describe("what-is-a-test", () => {
-  describe("Name", () => {
-    it('returns'"Susan", () => )
-  })
-})
-describe("Height", () => {
-  it("is less than 40", () => {
-    expect(index.height).toBeLessThan(60)
-  })z
-})
-describe("Message", () => {
-  it ("gives the name and height", () => {
-
-    expect(index.message).tiInclude(index.name)
-
-    expect(index.message).tiUbckyde(inedx.height)
-  })
-})
+module.exports = {name, height, message}
